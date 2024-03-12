6b5e313b8e6ef2f8c4b9a883a57bc92ac3c327cd
Cornbread2100 <cornbread2100@cornbread2100.com>
2024-03-11 21:36:46 -0400

    in-game player sync

--- client/host.js
+++ client/host.js
@@ -1,84 +1,212 @@
-const config = {
-    iceServers: [
-        {
-            urls: [
-                'stun:stun1.l.google.com:19302',
-                'stun:stun2.l.google.com:19302',
-            ],
-        },
-    ]
-}
-let peerLocal = new RTCPeerConnection(config);
-
-peerLocal.onconnectionstatechange = function(e) {
-    console.log('peerLocal', 'onconnectionstatechange', e);
-};
-peerLocal.ondatachannel = function(e) {
-    console.log('peerLocal', 'ondatachannel', e);
-};
-peerLocal.onsignalingstatechange = function(e) {
-    console.log('peerLocal', 'onsignalingstatechange', peerLocal.signalingState);
-};
-peerLocal.onicegatheringstatechange = function(e) {
-    console.log('peerLocal', 'onicegatheringstatechange', peerLocal.iceGatheringState);
-    if (peerLocal.iceGatheringState == 'complete') ws.close();
-};
-peerLocal.onicecandidate = function(e){
-    console.log('peerLocal', 'onicecandidate', e.candidate);
-    //Share ICE candidates with peerRemote
-    console.log('candidate', e.candidate)
-    if (e.candidate != null) ws.send(`\x00${JSON.stringify(e.candidate)}`);
-};
-peerLocal.onnegotiationneeded = function(e) {
-    console.log('peerLocal', 'onnegotiationneeded', e);
-};
-window.dcLocal = peerLocal.createDataChannel('rtcdc');
-window.dcLocal.onopen = function(e) {
-    console.log('dcLocal', 'onopen', e);
-    console.log('dcLocal.send("message") to send from server');
-};
-window.dcLocal.onmessage = function(e) {
-    console.log('dcLocal', 'onmessage', e.data);
-};
-window.dcLocal.onerror = function(e) {
-    console.error('dcLocal', 'onerror', e);
-};
-window.dcLocal.onclose = function(e) {
-    console.log('dcLocal', 'onclose', e);
-};
-
 (async () => {
-    console.log('peerLocal', 'createOffer');
-    let peerLocalOffer = await peerLocal.createOffer();
-
-    ws = new WebSocket('wss://n-gon.cornbread2100.com');
-    ws.onopen = async () => {
-        console.log('connected');
-        ws.send(`\x00${JSON.stringify(peerLocalOffer)}`);
-    }
-    let state = 0;
-    ws.onmessage = async (message) => {
-        console.log('message:', message.data)
-
-        if (message.data[0] == '\x00') {
-            if (state == 0) {
-                alert(`Join code: ${message.data.substring(1)}\nPress "ok" before entring it on another device.`);
-                state++;
-            } else {
-                const peerRemoteAnswer = new RTCSessionDescription(JSON.parse(message.data.substring(1)));
-                console.log('peerLocal', 'setLocalDescription', peerLocalOffer);
-                await peerLocal.setLocalDescription(peerLocalOffer);
-                console.log('peerLocal', 'setRemoteDescription', peerRemoteAnswer);
-                await peerLocal.setRemoteDescription(peerRemoteAnswer);
-            }
+    await new Promise(async (resolve, reject) => {
+        const config = {
+            iceServers: [
+                {
+                    urls: [
+                        'stun:stun1.l.google.com:19302',
+                        'stun:stun2.l.google.com:19302',
+                    ],
+                },
+            ]
         }
-        if (message.data[0] == '\x01') peerLocal.addIceCandidate(new RTCIceCandidate(JSON.parse(message.data.substring(1))));
-        if (message.data[0] == '\x02') console.error(message.data.substring(1));
+        let peerLocal = new RTCPeerConnection(config);
+
+        peerLocal.onconnectionstatechange = function(e) {
+            console.log('peerLocal', 'onconnectionstatechange', e);
+        };
+        peerLocal.ondatachannel = function(e) {
+            console.log('peerLocal', 'ondatachannel', e);
+        };
+        peerLocal.onsignalingstatechange = function(e) {
+            console.log('peerLocal', 'onsignalingstatechange', peerLocal.signalingState);
+        };
+        peerLocal.onicegatheringstatechange = function(e) {
+            console.log('peerLocal', 'onicegatheringstatechange', peerLocal.iceGatheringState);
+            if (peerLocal.iceGatheringState == 'complete') {
+                ws.close();
+                resolve();
+            }
+        };
+        peerLocal.onicecandidate = function(e){
+            console.log('peerLocal', 'onicecandidate', e.candidate);
+            //Share ICE candidates with peerRemote
+            console.log('candidate', e.candidate)
+            if (e.candidate != null) ws.send(`\x00${JSON.stringify(e.candidate)}`);
+        };
+        peerLocal.onnegotiationneeded = function(e) {
+            console.log('peerLocal', 'onnegotiationneeded', e);
+        };
+        window.dcLocal = peerLocal.createDataChannel('rtcdc');
+        window.dcLocal.onopen = function(e) {
+            console.log('dcLocal', 'onopen', e);
+            console.log('dcLocal.send("message") to send from server');
+        };
+        window.dcLocal.onmessage = function(message) {
+            console.log('dcLocal', 'onmessage', message.data);
+            const id = message.data[0];
+            let data = message.data.substring(1);
+            if (id == '\x00') {
+                data = JSON.parse(data);
+                for (const value in data) player2[value] = data[value];
+            }
+        };
+        window.dcLocal.onerror = function(e) {
+            console.error('dcLocal', 'onerror', e);
+        };
+        window.dcLocal.onclose = function(e) {
+            console.log('dcLocal', 'onclose', e);
+        };
+
+        console.log('peerLocal', 'createOffer');
+        let peerLocalOffer = await peerLocal.createOffer();
+
+        ws = new WebSocket('wss://n-gon.cornbread2100.com');
+        ws.onopen = async () => {
+            console.log('connected');
+            ws.send(`\x00${JSON.stringify(peerLocalOffer)}`);
+        }
+        let state = 0;
+        ws.onmessage = async (message) => {
+            console.log('message:', message.data)
+
+            if (message.data[0] == '\x00') {
+                if (state == 0) {
+                    alert(`Join code: ${message.data.substring(1)}\nPress "ok" before entring it on another device.`);
+                    state++;
+                } else {
+                    const peerRemoteAnswer = new RTCSessionDescription(JSON.parse(message.data.substring(1)));
+                    console.log('peerLocal', 'setLocalDescription', peerLocalOffer);
+                    await peerLocal.setLocalDescription(peerLocalOffer);
+                    console.log('peerLocal', 'setRemoteDescription', peerRemoteAnswer);
+                    await peerLocal.setRemoteDescription(peerRemoteAnswer);
+                }
+            }
+            if (message.data[0] == '\x01') peerLocal.addIceCandidate(new RTCIceCandidate(JSON.parse(message.data.substring(1))));
+            if (message.data[0] == '\x02') console.error(message.data.substring(1));
+        }
+        ws.onerror = (err) => {
+            console.error(err);
+        }
+        ws.onclose = () => {
+            console.log('disconnected');
+        }
+    });
+
+    let player2 = {
+        angle: 0,
+        bodyGradient: null,
+        calcLeg: (cycle_offset, offset) => {
+            player2.hip.x = 12 + offset;
+            player2.hip.y = 24 + offset;
+            //stepSize goes to zero if Vx is zero or not on ground (make m transition cleaner)
+            player2.stepSize = 0.8 * player2.stepSize + 0.2 * (7 * Math.sqrt(Math.min(9, Math.abs(player2.Vx))) * player2.onGround);
+            //changes to stepsize are smoothed by adding only a percent of the new value each cycle
+            const stepAngle = 0.034 * player2.walk_cycle + cycle_offset;
+            player2.foot.x = 2.2 * player2.stepSize * Math.cos(stepAngle) + offset;
+            player2.foot.y = offset + 1.2 * player2.stepSize * Math.sin(stepAngle) + player2.yOff + player2.height;
+            const Ymax = player2.yOff + player2.height;
+            if (player2.foot.y > Ymax) player2.foot.y = Ymax;
+    
+            //calculate knee position as intersection of circle from hip and foot
+            const d = Math.sqrt((player2.hip.x - player2.foot.x) * (player2.hip.x - player2.foot.x) + (player2.hip.y - player2.foot.y) * (player2.hip.y - player2.foot.y));
+            const l = (player2.legLength1 * player2.legLength1 - player2.legLength2 * player2.legLength2 + d * d) / (2 * d);
+            const h = Math.sqrt(player2.legLength1 * player2.legLength1 - l * l);
+            player2.knee.x = (l / d) * (player2.foot.x - player2.hip.x) - (h / d) * (player2.foot.y - player2.hip.y) + player2.hip.x + offset;
+            player2.knee.y = (l / d) * (player2.foot.y - player2.hip.y) + (h / d) * (player2.foot.x - player2.hip.x) + player2.hip.y;
+        },
+        color: { hue: 0, sat: 0, light: 100 },
+        drawLeg: (stroke) => {
+            if (player2.angle > -Math.PI / 2 && player2.angle < Math.PI / 2) {
+                player2.flipLegs = 1;
+            } else {
+                player2.flipLegs = -1;
+            }
+            ctx.save();
+            ctx.scale(player2.flipLegs, 1); //leg lines
+            ctx.beginPath();
+            ctx.moveTo(player2.hip.x, player2.hip.y);
+            ctx.lineTo(player2.knee.x, player2.knee.y);
+            ctx.lineTo(player2.foot.x, player2.foot.y);
+            ctx.strokeStyle = stroke;
+            ctx.lineWidth = 6;
+            ctx.stroke();
+
+            //toe lines
+            ctx.beginPath();
+            ctx.moveTo(player2.foot.x, player2.foot.y);
+            ctx.lineTo(player2.foot.x - 14, player2.foot.y + 5);
+            ctx.moveTo(player2.foot.x, player2.foot.y);
+            ctx.lineTo(player2.foot.x + 14, player2.foot.y + 5);
+            ctx.lineWidth = 4;
+            ctx.stroke();
+
+            //hip joint
+            ctx.beginPath();
+            ctx.arc(player2.hip.x, player2.hip.y, 9, 0, 2 * Math.PI);
+            //knee joint
+            ctx.moveTo(player2.knee.x + 5, player2.knee.y);
+            ctx.arc(player2.knee.x, player2.knee.y, 5, 0, 2 * Math.PI);
+            //foot joint
+            ctx.moveTo(player2.foot.x + 4, player2.foot.y + 1);
+            ctx.arc(player2.foot.x, player2.foot.y + 1, 4, 0, 2 * Math.PI);
+            ctx.fillStyle = player2.fillColor;
+            ctx.fill();
+            ctx.lineWidth = 2;
+            ctx.stroke();
+            ctx.restore();
+        },
+        fillColor: null,
+        fillColorDark: null,
+        flipLegs: -1,
+        foot: { x: 0, y: 0 },
+        height: 42,
+        hip: { x: 12, y: 24 },
+        immuneCycle: 0,
+        knee: { x: 0, y: 0, x2: 0, y2: 0 },
+        legLength1: 55,
+        legLength2: 45,
+        onGround: false,
+        pos: { x: 0, y: 0 },
+        stepSize: 0,
+        Vx: 0,
+        Vy: 0,
+        walk_cycle: 0,
+        yOff: 70
     }
-    ws.onerror = (err) => {
-        console.error(err);
-    }
-    ws.onclose = () => {
-        console.log('disconnected');
+    player2.fillColor = `hsl(${player2.color.hue},${player2.color.sat}%,${player2.color.light}%)`;
+    player2.fillColorDark = `hsl(${player2.color.hue},${player2.color.sat}%,${player2.color.light - 25}%)`;
+    let grd = ctx.createLinearGradient(-30, 0, 30, 0);
+    grd.addColorStop(0, player2.fillColorDark);
+    grd.addColorStop(1, player2.fillColor);
+    player2.bodyGradient = grd;
+
+    const oldStartGame = simulation.startGame;
+    simulation.startGame = () => {
+        oldStartGame();
+        simulation.ephemera.push({ name: 'Player2', count: 0, do: () => {
+            ctx.fillStyle = player2.fillColor;
+            ctx.save();
+            ctx.globalAlpha = (player2.immuneCycle < m.cycle) ? 1 : 0.5;
+            ctx.translate(player2.pos.x, player2.pos.y);
+            player2.calcLeg(Math.PI, -3);
+            player2.drawLeg("#4a4a4a");
+            player2.calcLeg(0, 0);
+            player2.drawLeg("#333");
+            ctx.rotate(player2.angle);
+            ctx.beginPath();
+            ctx.arc(0, 0, 30, 0, 2 * Math.PI);
+            ctx.fillStyle = player2.bodyGradient;
+            ctx.fill();
+            ctx.arc(15, 0, 4, 0, 2 * Math.PI);
+            ctx.strokeStyle = "#333";
+            ctx.lineWidth = 2;
+            ctx.stroke();
+            ctx.restore();
+            powerUps.boost.draw();
+        }})
+        simulation.ephemera.push({ name: 'Movement Broadcast', count: 0, do: () => {
+            dcLocal.send(`\x00${JSON.stringify({ angle: m.angle, pos: m.pos, onGround: m.onGround, Vx: m.Vx, walk_cycle: m.walk_cycle, yOff: m.yOff })}`);
+        }})
     }
 })();
--- client/join.js
+++ client/join.js
@@ -1,80 +1,207 @@
-const config = {
-    iceServers: [
-        {
-            urls: [
-                'stun:stun1.l.google.com:19302',
-                'stun:stun2.l.google.com:19302',
-            ],
+(async () => {
+    await new Promise((resolve, reject) => {
+        const config = {
+            iceServers: [
+                {
+                    urls: [
+                        'stun:stun1.l.google.com:19302',
+                        'stun:stun2.l.google.com:19302',
+                    ],
+                },
+            ]
+        }
+        let peerRemote = new RTCPeerConnection(config);
+
+        peerRemote.onconnectionstatechange = function(e) {
+            console.log('peerRemote', 'onconnectionstatechange', e);
+        };
+        peerRemote.ondatachannel = function(e) {
+            // peerLocal started a data channel, so connect to it here
+            console.log('peerRemote', 'ondatachannel', e);
+            window.dcRemote = e.channel;
+            window.dcRemote.onopen = function(e) {
+                console.log('dcRemote', 'onopen', e);
+                console.log('dcRemote.send("message") to send from remote');
+            };
+            window.dcRemote.onmessage = function(message) {
+                console.log('dcRemote', 'onmessage', message.data);
+                const id = message.data[0];
+                let data = message.data.substring(1);
+                if (id == '\x00') {
+                    data = JSON.parse(data);
+                    for (const value in data) player1[value] = data[value];
+                }
+            };
+            window.dcRemote.onerror = function(e) {
+                console.error('dcRemote', 'onerror', e);
+            };
+            window.dcRemote.onclose = function(e) {
+                console.log('dcRemote', 'onclose', e);
+            };
+
+        };
+        peerRemote.onsignalingstatechange = function(e) {
+            console.log('peerRemote', 'onsignalingstatechange', peerRemote.signalingState);
+            if (peerRemote.iceGatheringState == 'complete') ws.close();
+        };
+        peerRemote.onicegatheringstatechange = function(e) {
+            console.log('peerRemote', 'onicegatheringstatechange', peerRemote.iceGatheringState);
+        };
+        peerRemote.onicecandidate = function(e){
+            //Share ICE candidates with peerLocal
+            console.log('peerRemote', 'onicecandidate', e);
+            if (e.candidate != null) ws.send(`\x01${JSON.stringify(e.candidate)}`);
+        };
+        peerRemote.onnegotiationneeded = function(e) {
+            console.log('peerRemote', 'onnegotiationneeded', e);
+        };
+
+        ws = new WebSocket('wss://n-gon.cornbread2100.com');
+        ws.onopen = async () => {
+            console.log('connected');
+            ws.send(`\x01${prompt('Join code:')}`)
+        }
+        ws.onmessage = async (message) => {
+            console.log('message:', message.data, message.data[0] == '\x01')
+
+            if (message.data[0] == '\x00') {
+                const peerLocalOffer = new RTCSessionDescription(JSON.parse(message.data.substring(1)));
+                console.log('peerRemote', 'setRemoteDescription', peerLocalOffer);
+                await peerRemote.setRemoteDescription(peerLocalOffer);
+
+                console.log('peerRemote', 'setRemoteDescription');
+                let peerRemoteAnswer = await peerRemote.createAnswer();
+
+                console.log('peerRemote', 'setLocalDescription', peerRemoteAnswer);
+                await peerRemote.setLocalDescription(peerRemoteAnswer);
+                ws.send(`\x01${JSON.stringify(peerRemoteAnswer)}`);
+            }
+            if (message.data[0] == '\x01') peerRemote.addIceCandidate(new RTCIceCandidate(JSON.parse(message.data.substring(1))));
+            if (message.data[0] == '\x02') console.error(message.data.substring(1));
+        }
+        ws.onerror = (err) => {
+            console.error(err);
+        }
+        ws.onclose = () => {
+            console.log('disconnected');
+            resolve();
+        }
+    })
+
+    let player1 = {
+        angle: 0,
+        bodyGradient: null,
+        calcLeg: (cycle_offset, offset) => {
+            player1.hip.x = 12 + offset;
+            player1.hip.y = 24 + offset;
+            //stepSize goes to zero if Vx is zero or not on ground (make m transition cleaner)
+            player1.stepSize = 0.8 * player1.stepSize + 0.2 * (7 * Math.sqrt(Math.min(9, Math.abs(player1.Vx))) * player1.onGround);
+            //changes to stepsize are smoothed by adding only a percent of the new value each cycle
+            const stepAngle = 0.034 * player1.walk_cycle + cycle_offset;
+            player1.foot.x = 2.2 * player1.stepSize * Math.cos(stepAngle) + offset;
+            player1.foot.y = offset + 1.2 * player1.stepSize * Math.sin(stepAngle) + player1.yOff + player1.height;
+            const Ymax = player1.yOff + player1.height;
+            if (player1.foot.y > Ymax) player1.foot.y = Ymax;
+    
+            //calculate knee position as intersection of circle from hip and foot
+            const d = Math.sqrt((player1.hip.x - player1.foot.x) * (player1.hip.x - player1.foot.x) + (player1.hip.y - player1.foot.y) * (player1.hip.y - player1.foot.y));
+            const l = (player1.legLength1 * player1.legLength1 - player1.legLength2 * player1.legLength2 + d * d) / (2 * d);
+            const h = Math.sqrt(player1.legLength1 * player1.legLength1 - l * l);
+            player1.knee.x = (l / d) * (player1.foot.x - player1.hip.x) - (h / d) * (player1.foot.y - player1.hip.y) + player1.hip.x + offset;
+            player1.knee.y = (l / d) * (player1.foot.y - player1.hip.y) + (h / d) * (player1.foot.x - player1.hip.x) + player1.hip.y;
         },
-    ]
-}
-let peerRemote = new RTCPeerConnection(config);
+        color: { hue: 0, sat: 0, light: 100 },
+        drawLeg: (stroke) => {
+            if (player1.angle > -Math.PI / 2 && player1.angle < Math.PI / 2) {
+                player1.flipLegs = 1;
+            } else {
+                player1.flipLegs = -1;
+            }
+            ctx.save();
+            ctx.scale(player1.flipLegs, 1); //leg lines
+            ctx.beginPath();
+            ctx.moveTo(player1.hip.x, player1.hip.y);
+            ctx.lineTo(player1.knee.x, player1.knee.y);
+            ctx.lineTo(player1.foot.x, player1.foot.y);
+            ctx.strokeStyle = stroke;
+            ctx.lineWidth = 6;
+            ctx.stroke();
 
-peerRemote.onconnectionstatechange = function(e) {
-    console.log('peerRemote', 'onconnectionstatechange', e);
-};
-peerRemote.ondatachannel = function(e) {
-    // peerLocal started a data channel, so connect to it here
-    console.log('peerRemote', 'ondatachannel', e);
-    window.dcRemote = e.channel;
-    window.dcRemote.onopen = function(e) {
-        console.log('dcRemote', 'onopen', e);
-        console.log('dcRemote.send("message") to send from remote');
+            //toe lines
+            ctx.beginPath();
+            ctx.moveTo(player1.foot.x, player1.foot.y);
+            ctx.lineTo(player1.foot.x - 14, player1.foot.y + 5);
+            ctx.moveTo(player1.foot.x, player1.foot.y);
+            ctx.lineTo(player1.foot.x + 14, player1.foot.y + 5);
+            ctx.lineWidth = 4;
+            ctx.stroke();
 
-    };
-    window.dcRemote.onmessage = function(e) {
-        console.log('dcRemote', 'onmessage', e.data);
-    };
-    window.dcRemote.onerror = function(e) {
-        console.error('dcRemote', 'onerror', e);
-    };
-    window.dcRemote.onclose = function(e) {
-        console.log('dcRemote', 'onclose', e);
-    };
-
-};
-peerRemote.onsignalingstatechange = function(e) {
-    console.log('peerRemote', 'onsignalingstatechange', peerRemote.signalingState);
-    if (peerRemote.iceGatheringState == 'complete') ws.close();
-};
-peerRemote.onicegatheringstatechange = function(e) {
-    console.log('peerRemote', 'onicegatheringstatechange', peerRemote.iceGatheringState);
-};
-peerRemote.onicecandidate = function(e){
-    //Share ICE candidates with peerLocal
-    console.log('peerRemote', 'onicecandidate', e);
-    if (e.candidate != null) ws.send(`\x01${JSON.stringify(e.candidate)}`);
-};
-peerRemote.onnegotiationneeded = function(e) {
-    console.log('peerRemote', 'onnegotiationneeded', e);
-};
-
-ws = new WebSocket('wss://n-gon.cornbread2100.com');
-ws.onopen = async () => {
-    console.log('connected');
-    ws.send(`\x01${prompt('Join code:')}`)
-}
-ws.onmessage = async (message) => {
-    console.log('message:', message.data, message.data[0] == '\x01')
-
-    if (message.data[0] == '\x00') {
-        const peerLocalOffer = new RTCSessionDescription(JSON.parse(message.data.substring(1)));
-        console.log('peerRemote', 'setRemoteDescription', peerLocalOffer);
-        await peerRemote.setRemoteDescription(peerLocalOffer);
-
-        console.log('peerRemote', 'setRemoteDescription');
-        let peerRemoteAnswer = await peerRemote.createAnswer();
-
-        console.log('peerRemote', 'setLocalDescription', peerRemoteAnswer);
-        await peerRemote.setLocalDescription(peerRemoteAnswer);
-        ws.send(`\x01${JSON.stringify(peerRemoteAnswer)}`);
+            //hip joint
+            ctx.beginPath();
+            ctx.arc(player1.hip.x, player1.hip.y, 9, 0, 2 * Math.PI);
+            //knee joint
+            ctx.moveTo(player1.knee.x + 5, player1.knee.y);
+            ctx.arc(player1.knee.x, player1.knee.y, 5, 0, 2 * Math.PI);
+            //foot joint
+            ctx.moveTo(player1.foot.x + 4, player1.foot.y + 1);
+            ctx.arc(player1.foot.x, player1.foot.y + 1, 4, 0, 2 * Math.PI);
+            ctx.fillStyle = player1.fillColor;
+            ctx.fill();
+            ctx.lineWidth = 2;
+            ctx.stroke();
+            ctx.restore();
+        },
+        fillColor: null,
+        fillColorDark: null,
+        flipLegs: -1,
+        foot: { x: 0, y: 0 },
+        height: 42,
+        hip: { x: 12, y: 24 },
+        immuneCycle: 0,
+        knee: { x: 0, y: 0, x2: 0, y2: 0 },
+        legLength1: 55,
+        legLength2: 45,
+        onGround: false,
+        pos: { x: 0, y: 0 },
+        stepSize: 0,
+        Vx: 0,
+        Vy: 0,
+        walk_cycle: 0,
+        yOff: 70
     }
-    if (message.data[0] == '\x01') peerRemote.addIceCandidate(new RTCIceCandidate(JSON.parse(message.data.substring(1))));
-    if (message.data[0] == '\x02') console.error(message.data.substring(1));
-}
-ws.onerror = (err) => {
-    console.error(err);
-}
-ws.onclose = () => {
-    console.log('disconnected');
-}
+    player1.fillColor = `hsl(${player1.color.hue},${player1.color.sat}%,${player1.color.light}%)`
+    player1.fillColorDark = `hsl(${player1.color.hue},${player1.color.sat}%,${player1.color.light - 25}%)`;
+    let grd = ctx.createLinearGradient(-30, 0, 30, 0);
+    grd.addColorStop(0, player1.fillColorDark);
+    grd.addColorStop(1, player1.fillColor);
+    player1.bodyGradient = grd;
+
+    const oldStartGame = simulation.startGame;
+    simulation.startGame = () => {
+        oldStartGame();
+        simulation.ephemera.push({ name: 'Player2', count: 0, do: () => {
+            ctx.fillStyle = player1.fillColor;
+            ctx.save();
+            ctx.globalAlpha = (player1.immuneCycle < m.cycle) ? 1 : 0.5;
+            ctx.translate(player1.pos.x, player1.pos.y);
+            player1.calcLeg(Math.PI, -3);
+            player1.drawLeg("#4a4a4a");
+            player1.calcLeg(0, 0);
+            player1.drawLeg("#333");
+            ctx.rotate(player1.angle);
+            ctx.beginPath();
+            ctx.arc(0, 0, 30, 0, 2 * Math.PI);
+            ctx.fillStyle = player1.bodyGradient;
+            ctx.fill();
+            ctx.arc(15, 0, 4, 0, 2 * Math.PI);
+            ctx.strokeStyle = "#333";
+            ctx.lineWidth = 2;
+            ctx.stroke();
+            ctx.restore();
+            powerUps.boost.draw();
+        }})
+        simulation.ephemera.push({ name: 'Movement Broadcast', count: 0, do: () => {
+            dcRemote.send(`\x00${JSON.stringify({ angle: m.angle, pos: m.pos, onGround: m.onGround, Vx: m.Vx, walk_cycle: m.walk_cycle, yOff: m.yOff })}`);
+        }})
+    }
+})();
